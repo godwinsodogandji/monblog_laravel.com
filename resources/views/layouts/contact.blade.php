0b5ad18a0d26c8904c67a002e148f8e00d9c398e
godwinsodogandji <jeansodogandji@gmail.com>
2024-07-22 10:24:22 +0100

    contact crée

---
+++ resources/views/layouts/contact.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master')
+
+@section('contenu')
+<h2>Contactez-nous</h2>
+@endsection
+
+
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mon Blog laravel</title>
+</head>
+<body>
+        <h1>Mon Blog laravel</h1>
+       <p> <a href="/contact-us">Contactez-nous</a></p>
+
+       {{-- Contenu de toutes les pages ici --}}
+    @yield('contenu')
+
+
+
+</body>
+</html>
+
+
