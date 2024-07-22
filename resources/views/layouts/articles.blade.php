3d6d7bc87f0cc19e6e23b8fce4bfec8217611a0f
godwinsodogandji <jeansodogandji@gmail.com>
2024-07-22 12:35:20 +0100

    @if @else

--- resources/views/layouts/articles.blade.php
+++ resources/views/layouts/articles.blade.php
@@ -6,4 +6,14 @@
 
 @section('contenu')
     <h2>Mes articles</h2>
+    @if ($articles)
+        @foreach ($articles as $article)
+            <article>
+                <h2>{{ $article['title'] }}</h2>
+                <p>{{ $article['body'] }}</p>
+            </article>
+        @endforeach
+        @else
+        <p>Oop! 😢! Aucun article trouvé.</p>
+    @endif
 @endsection
